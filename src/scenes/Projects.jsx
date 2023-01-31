85d998bdac9c52479467105727d7ba310e5366ee
juan jose zabala <juanzabala317@hotmail.com>
2023-01-31 15:41:45 -0500

    cuarto avance

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,15 +1,34 @@
+import { motion } from "framer-motion";
 import LineGradient from "../components/LineGradient";
 
 const container = {
-    hidden: {},
-    visible: {
-        transition: { staggerChildren: 0.2 }
-    }
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
 };
 
 const projectVariant = {
-    hidden: { opacity: 0, scale: 0.8},
-    visible: { opacity: 1, scale: 1}
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+function Project({ title }) {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+  bg-gray z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. nulla
+          porttitor accumsan tincidunt.
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+    </motion.div>
+  );
 }
 
 function Projects() {
@@ -37,19 +56,43 @@ function Projects() {
         </div>
         <p className="mt-10 mb-10">
           Aliquam, amet dui feugiat fecilisi dui. Aliquam aliquet integer ut
-          fames odio in at. At magna ornare dictum lectus. Purus massa morbi 
+          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
           purus nec eget eleifend ut elit.
         </p>
       </motion.div>
       {/* PROJECTS */}
       <div className="flex justify-center">
-      <motion.div
-        className="sm:grid sm:grid-cols-3"
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        variants={container}
-        ></motion.div>
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {/* ROW 1 */}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-red
+           max-w-[400px] max-h-{400px] text-2xl font-playfair font-semibold"
+          >
+            BEAUTIFUL USER INTERFACES
+          </div>
+          <Project title="Project 1" />
+          <Project title="Project 2" />
+
+          {/* ROW 2 */}
+          <Project title="Project 3" />
+          <Project title="Project 4" />
+          <Project title="Project 5" />
+          {/* ROW 3 */}
+          <Project title="Project 6" />
+          <Project title="Project 7" />
+          <div
+            className="flex justify-center text-center items-center p-10 bg-blue
+           max-w-[400px] max-h-{400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div>
+        </motion.div>
       </div>
     </section>
   );
